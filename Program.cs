914d596e55ae8aa9a921457f32c55404e3d1900c
Samuel Feese <samuelfeese@outlook.com>
2019-07-16 12:00:09 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] xs = new int[7] { 1, 2, 3, 4, 5, 6, 7 };
+            List<int> xs = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+
+            for (int i = 0; i < xs.Count -1; ++i)
+            {
+                xs[i] = xs[i + 1];
+            }
+
+
+        }
+
+        static void Printcollection<b>(List<b> toPrint) => Printcollection(toPrint.ToArray());
+        static void Printcollection<a>(a[] toPrint)
+        {
+            foreach (var x in toPrint)
+            {
+                Console.WriteLine($"{x}, ");
+            }
+        }
+    }
+}
